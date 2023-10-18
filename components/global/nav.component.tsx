3a08c195380e34cfb99599460a5e59189fdc4d14
devil1321 <s.dominik1321@gmail.com>
2023-10-18 16:36:33 +0200

    init nav

--- components/global/nav.component.tsx
+++ components/global/nav.component.tsx
@@ -12,6 +12,8 @@ interface ItemState {
 
 const Nav = () => {
 
+  const [index,setIndex] = useState<number>(0)
+  const [isMenu,setIsMenu] = useState<boolean>(false)
   const [menuItems,setMenuItems] = useState<any[]>([
     [
       {
@@ -58,46 +60,71 @@ const Nav = () => {
    
   ])
 
+
+  const handleCategory = (category:string) =>{
+    switch(category){
+      case 'men':
+         setIndex(0)
+         setIsMenu(true)
+        break
+      case 'woman':
+         setIndex(1)
+         setIsMenu(true)
+        break
+      case 'accessories':
+         setIndex(2)
+         setIsMenu(true)
+        break
+      default:
+        setIndex(0)
+        setIsMenu(true)
+    }
+  }
+
   return (
     <div className={styles.nav}>
       <div className={styles.header}>Free US shipping on order $80+</div>
-      <div className={styles.navigation}>
-        <div className={styles.logo}>
-          <img src="/assets/logos/sopa-logo-black.png" alt="logo" />
+      <div className={styles.nav_inner_wrapper}>
+        <div className={styles.navigation}>
+          <div className={styles.logo}>
+            <img src="/assets/logos/sopa-logo-black.png" alt="logo" />
+          </div>
+          <div className={styles.menu}> 
+            <Link onClick={()=>handleCategory('men')} className='u1' href="#">Men</Link>
+            <Link onClick={()=>handleCategory('woman')} className='u1' href="#">Woman</Link>
+            <Link onClick={()=>handleCategory('accessories')} className='u1' href="#">Accessories</Link>
+            <Link className='u1' href="/about">About</Link>
+            <Link className='u1' href="/quiz">Some Finder Quiz</Link>
+          </div>
+          <div className={styles.controls}>
+            <Link href="/help">Support</Link>
+            <img src="/assets/icons/user.png" alt="user" />
+            <img src="/assets/icons/cart.png" alt="user" />
+          </div>
         </div>
-        <div className={styles.menu}> 
-        </div>
-          <Link href="#">Men</Link>
-          <Link href="#">Woman</Link>
-          <Link href="#">Accessories</Link>
-          <Link href="/about">About</Link>
-          <Link href="/quiz">Some Finder Quiz</Link>
-      </div>
-      <div className={styles.controls}>
-        <Link href="/help">Support</Link>
-        <img src="/assets/icons/user.png" alt="user" />
-        <img src="/assets/icons/cart.png" alt="user" />
-      </div>
-      <div className={styles.main_menu_wrapper}>
-        <div className={styles.main_menu}>
-          {menuItems.map((section:any) => {
-            return section.map((i:ItemState) => <Item key={i.id} item={i} />)
-          })}
-        </div>
-        <h3>Not Sure?</h3>
-        <Link href="/quiz">Takse the Shoe Finder Quiz</Link>
+        {isMenu && 
+          <div className={styles.main_menu_wrapper}>
+            <div className={styles.main_menu}>
+              {menuItems[index].map((i:ItemState) => <Item key={i.id} item={i} />)}
+            </div>
+            <p className='p4'>Not Sure?</p>
+            <Link href="/quiz">Take the Shoe Finder Quiz</Link>
+          </div>}
       </div>
     </div>
+
   )
 }
 
-const Item:React.FC<any> = ({item}) => {
+const Item:React.FC<{item:ItemState}> = ({item}) => {
   return(
-    <div className={styles.item}>
-      <img src={item.img} alt="item-image" />
-      {item?.name && <button>{item?.name}</button>}
-      <h3><span>{item.footer}</span> {item?.price && <span>{item?.price}$</span>}</h3>
-    </div>
+    <Link href="#" as="#" className={styles.item}>
+      <div>
+        <img src={item.img} alt="item-image" />
+        {item?.name && <button>{item?.name}</button>}
+        <p className='p4'><span>{item.footer}</span> {item?.price && <span>{item?.price}$</span>}</p>
+      </div>
+    </Link>
   )
 }
 
