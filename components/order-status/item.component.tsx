26236db8cdd8f148c9614e0b589a1cb83ba6fb45
devil1321 <s.dominik1321@gmail.com>
2023-10-28 16:01:04 +0200

    order status desktop completed

--- components/order-status/item.component.tsx
+++ components/order-status/item.component.tsx
@@ -8,7 +8,22 @@ interface ItemProps{
 const Item:React.FC<ItemProps> = ({product}) => {
   return (
     <div className={styles.item}>
-      
+      <div className={styles.img}>
+        <img src={product.details_images[0]} alt="product-image" />
+      </div>
+      <div className={styles.details}>
+        <h3 className="h8">{product.name}</h3>
+        <p className='p5'>{product.colors[0].name}</p>
+        <div className={styles.color} style={{backgroundColor:product.colors[0].color}}></div>
+        <div className={styles.quantity}>
+          <p className="p5">Items</p>
+          <div className={styles.quantity_controller}>
+            <div className='p4'>+</div>
+            <div className='p4'>1</div>
+            <div className='p4'>-</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- components/order-status/orders.component.tsx
+++ components/order-status/orders.component.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import styles from '@/styles/components/order-status/orders.module.scss'
+import Link from 'next/link'
 
 const Orders = () => {
   return (
-    <div>
-      
+    <div className={styles.orders}>
+      <h2 className="h5">Your Orders</h2>
+      <p><span>0 orders placed in</span><span>Last 45 days <img src="/assets/icons/chevron-down.png" alt="chevron-down" /></span></p>
+      <p>You haven’t placed any orders in this period.</p>
+      <Link href="/" className='p5'>Start Shipping</Link>
     </div>
   )
 }
