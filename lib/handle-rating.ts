a71a39fb065a65bff59a2814fe7bff697771d865
devil1321 <s.dominik1321@gmail.com>
2023-11-08 12:14:39 +0100

    init checkout

--- lib/handle-rating.ts
+++ lib/handle-rating.ts
@@ -1,4 +1,4 @@
-export default function(product:any){
+export default function handleRating(product:any){
     let rating:string[] = []
     for(let i = 1; i <= Math.round(product.rating); i++){
         const str = 'rating-' + product.rating
