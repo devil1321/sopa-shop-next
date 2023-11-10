d7f2bef1d2fd16df36a4493d65b3ae0869a4f416
devil1321 <s.dominik1321@gmail.com>
2023-11-10 12:05:21 +0100

    init payment and shipping

--- pages/checkout/payment.tsx
+++ pages/checkout/payment.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { InformationComponents } from '@/components/checkout/information'
+import styles from '@/styles/pages/checkout/payment.module.scss'
 
 const Payment = () => {
   return (
-    <div>
-      
+    <div className={styles.payment}>
+      <div className={styles.column}></div>
+      <div className={styles.column}>
+        <InformationComponents.Item />
+        <InformationComponents.GiftCard />
+        <InformationComponents.Summary />
+      </div>
     </div>
   )
 }
--- pages/checkout/shipping.tsx
+++ pages/checkout/shipping.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { InformationComponents } from '@/components/checkout/information'
+import styles from '@/styles/pages/checkout/shipping.module.scss'
 
 const Shipping = () => {
   return (
-    <div>
-      
+    <div className={styles.shipping}>
+      <div className={styles.column}></div>
+      <div className={styles.column}>
+        <InformationComponents.Item />
+        <InformationComponents.GiftCard />
+        <InformationComponents.Summary />
+      </div>
     </div>
   )
 }
